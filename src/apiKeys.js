e4d9165aafc5ba20fc2c0467842e2c79625ce49c
Pranay Kumar Parate <pranayparate53@gmail.com>
2024-08-10 19:17:44 +0530

    Initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "{590bac5389b1e6acd18c9cdd8bbeb87f}",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
